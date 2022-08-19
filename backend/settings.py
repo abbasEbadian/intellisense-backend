5ea8197acf595786e9c50ed3b55ddf6fa4ddad9c
abbasEbadian <abbasebadiann@gmail.com>
2022-08-19 14:42:42 +0430

    init

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+# Custom Apps
+
+INSTALLED_APPS += [
+] 
+
+# Third party Apps
+INSTALLED_APPS += [
+    'rest_framework',
+    'django_summernote',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -76,7 +87,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'intellisense.sqlite3',
     }
 }
 
@@ -116,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / STATIC_URL
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
